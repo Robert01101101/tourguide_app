eeed8129a3abba9151681f5c3ab154bc4f49dbd1
Robert01101101 <hi@rmichels.com>
2024-01-20 20:35:50 -0800

    More improvements to auth flow and cleaned up code

--- lib/signIn.dart
+++ lib/signIn.dart
@@ -15,9 +15,7 @@ import 'package:http/http.dart' as http;
 import 'uiElements/sign_in_button.dart';
 import 'package:tourguide_app/utilities/custom_import.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:tourguide_app/utilities/authProvider.dart' as myAuth;
-
-
+import 'package:tourguide_app/utilities/authProvider.dart' as my_auth;
 
 
 /// The SignIn app.
@@ -37,190 +35,91 @@ class _SignInState extends State<SignIn> {
   void initState() {
     super.initState();
 
-    //OLD
-    /*
-    _googleSignIn.onCurrentUserChanged
-        .listen((GoogleSignInAccount? account) async {
-// #docregion CanAccessScopes
-      // In mobile, being authenticated means being authorized...
-      bool isAuthorized = account != null;
-      // However, on web...
-      if (kIsWeb && account != null) {
-        print(' -- initState() - _googleSignIn.onCurrentUserChanged (web) -> account is null=${account == null}');
-        isAuthorized = await _googleSignIn.canAccessScopes(scopes);
-      }
-// #enddocregion CanAccessScopes
-
-      setState(() {
-        print(' -- initState() - _googleSignIn.onCurrentUserChanged (web) setState() - isAuthorized=${isAuthorized}');
-        _currentUser = account;
-        MyGlobals.user = _currentUser;
-        _isAuthorized = isAuthorized;
-      });
-
-      // Now that we know that the user can access the required scopes, the app
-      // can call the REST API.
-      if (isAuthorized) {
-        //unawaited(_handleGetContact(account!)); //CONTACT DEMO
-        myAuth.AuthProvider authProvider = Provider.of(context);
-        await authProvider.signInWithFirebase(account!); //from chatgpt
-      } else {
-        _handleAuthorizeScopes();
-      }
-    });*/
-
-    // In the web, _googleSignIn.signInSilently() triggers the One Tap UX.
-    //
-    // It is recommended by Google Identity Services to render both the One Tap UX
-    // and the Google Sign In button together to "reduce friction and improve
-    // sign-in rates" ([docs](https://developers.google.com/identity/gsi/web/guides/display-button#html)).
-    print(' -- initState() - _googleSignIn.signInSilently()');
-    myAuth.googleSignIn.signInSilently();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      my_auth.AuthProvider authProvider = Provider.of(context, listen: false);
+      authProvider.signInSilently();
+    });
   }
 
+  Widget _buildProcessingBody() {
+    my_auth.AuthProvider authProvider = Provider.of(context);
+    final GoogleSignInAccount? user = authProvider.user;
 
-
-  /* CONTACT DEMO
-  // Calls the People API REST endpoint for the signed-in user to retrieve information.
-  Future<void> _handleGetContact(GoogleSignInAccount user) async {
-    setState(() {
-      _contactText = 'Loading contact info...';
-    });
-    final http.Response response = await http.get(
-      Uri.parse('https://people.googleapis.com/v1/people/me/connections'
-          '?requestMask.includeField=person.names'),
-      headers: await user.authHeaders,
+    return Center(
+      child: SizedBox(
+        height: 100,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const CircularProgressIndicator(
+              semanticsLabel: 'Circular progress indicator',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            if (!authProvider.isLoggingOut) ...<Widget>[
+              const Text('Signed in successfully.\nConnecting to servers...'),
+            ],
+            if (authProvider.isLoggingOut) ...<Widget>[
+              const Text('Signing Out...'),
+            ],
+          ],
+        ),
+      ),
     );
-    if (response.statusCode != 200) {
-      setState(() {
-        _contactText = 'People API gave a ${response.statusCode} '
-            'response. Check logs for details.';
-      });
-      print('People API ${response.statusCode} response: ${response.body}');
-      return;
-    }
-    final Map<String, dynamic> data =
-    json.decode(response.body) as Map<String, dynamic>;
-    final String? namedContact = _pickFirstNamedContact(data);
-    setState(() {
-      if (namedContact != null) {
-        _contactText = 'I see you know $namedContact!';
-      } else {
-        _contactText = 'No contacts to display.';
-      }
-    });
   }
 
-  String? _pickFirstNamedContact(Map<String, dynamic> data) {
-    final List<dynamic>? connections = data['connections'] as List<dynamic>?;
-    // Generate a random index within the bounds of the connections list
-    final int randomIndex = Random().nextInt(connections!.length);
-
-    final Map<String, dynamic>? contact = connections[randomIndex] as Map<String, dynamic>?;
-    if (contact != null) {
-      final List<dynamic> names = contact['names'] as List<dynamic>;
-      final Map<String, dynamic>? name = names.firstWhere(
-            (dynamic name) =>
-        (name as Map<Object?, dynamic>)['displayName'] != null,
-        orElse: () => null,
-      ) as Map<String, dynamic>?;
-      if (name != null) {
-        return name['displayName'] as String?;
-      }
-    }
-    return null;
-  } CONTACT DEMO END */
-
-
-
-  // #enddocregion SignIn
-
-
-
-  Future<void> _handleSignOut() =>  myAuth.googleSignIn.disconnect();
-
-  Widget _buildBody() {
-    myAuth.AuthProvider authProvider = Provider.of(context);
+  Widget _buildButtonBody() {
+    my_auth.AuthProvider authProvider = Provider.of(context);
     final GoogleSignInAccount? user = authProvider.user;
     if (user != null) {
       // The user is Authenticated
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          ListTile(
-            leading: GoogleUserCircleAvatar(
-              identity: user,
-            ),
-            title: Text(user.displayName ?? ''),
-            subtitle: Text(user.email),
-          ),
-          const Text('Signed in successfully.'),
-
-          /*if (_isAuthorized) ...<Widget>[  //CONTACT DEMO
-            // The user has Authorized all required scopes
-            Text(_contactText),
-            ElevatedButton(
-              child: const Text('REFRESH'),
-              onPressed: () => _handleGetContact(user),
-            ),
-          ],*/
-          if (!authProvider.isAuthorized) ...<Widget>[
-            // The user has NOT Authorized all required scopes.
-            // (Mobile users may never see this button!)
-            const Text('Additional permissions needed to read your contacts.'),
-            ElevatedButton(
-              onPressed: authProvider.handleAuthorizeScopes,
-              child: const Text('REQUEST PERMISSIONS'),
-            ),
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            if (!authProvider.isAuthorized) ...<Widget>[
+              // The user has NOT Authorized all required scopes.
+              // (Mobile users may never see this button!)
+              const Text('Additional permissions needed to authorize your account.'),
+              ElevatedButton(
+                onPressed: authProvider.handleAuthorizeScopes,
+                child: const Text('REQUEST PERMISSIONS'),
+              ),
+            ],
           ],
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: _handleSignOut,
-                  child: const Text('SIGN OUT'),
-                ),
-                const SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: () => {
-                    CustomNavigationHelper.router.go(
-                      CustomNavigationHelper.explorePath,
-                    )
-                  },
-                  child: const Text('Proceed to app'),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       );
     } else {
       // The user is NOT Authenticated
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          const Text('You are not currently signed in.'),
-          // This method is used to separate mobile from web code with conditional exports.
-          // See: src/sign_in_button.dart
-          buildSignInButton(
-            onPressed: authProvider.handleSignIn,
-          ),
-        ],
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            const Text('You are signed out.'),
+            // This method is used to separate mobile from web code with conditional exports.
+            // See: src/sign_in_button.dart
+            buildSignInButton(
+              onPressed: authProvider.handleSignIn,
+            ),
+          ],
+        ),
       );
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    my_auth.AuthProvider authProvider = Provider.of(context);
+    final GoogleSignInAccount? user = authProvider.user;
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Google Sign In'),
+          title: const Text('Tourguide'),
         ),
         body: ConstrainedBox(
           constraints: const BoxConstraints.expand(),
-          child: _buildBody(),
+          child: ((user != null && authProvider.isAuthorized) || authProvider.isLoggingOut) ? _buildProcessingBody() : _buildButtonBody(),
         ));
   }
 }
--- lib/utilities/authProvider.dart
+++ lib/utilities/authProvider.dart
@@ -8,66 +8,52 @@ import 'package:tourguide_app/utilities/customNavigationHelper.dart';
 
 const List<String> scopes = <String>[
   'email',
-  //'https://www.googleapis.com/auth/contacts.readonly',  //CONTACT DEMO - for demo of using people API to get contacts etc
 ];
 
 GoogleSignIn googleSignIn = GoogleSignIn(
-  // Optional clientId
-  // clientId: 'your-client_id.apps.googleusercontent.com',
   scopes: scopes,
 );
 
-//with help from https://medium.com/@JigneshWorld/how-to-implement-an-authentication-feature-using-a-provider-in-flutter-1f351447d09d
+// AuthProvider handles login / logout, user auth state, and uses the provider
+// package and change notifier to share information with the widget tree
+//
+// Written with help from https://medium.com/@JigneshWorld/how-to-implement-an-authentication-feature-using-a-provider-in-flutter-1f351447d09d
 class AuthProvider with ChangeNotifier {
+//#region REGION: AuthProvider fields and constructor
   ////// PUBLIC /////
   GoogleSignInAccount? user; //(formerly _currentUser)
   late StreamSubscription userAuthSub;
   bool isAuthorized = false; // has granted permissions?
+  bool isLoggingOut = false;
 
   ////// PRIVATE /////
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-
-
+  ////// CONSTRUCTOR /////
   AuthProvider() {
-    /* //from Medium
-    userAuthSub =   FirebaseAuth.instance.onAuthStateChanged.listen((newUser) {
-      print('AuthProvider - FirebaseAuth - onAuthStateChanged - $newUser');
-      //user = newUser; //TODO
-      notifyListeners();
-    }, onError: (e) {
-      print('AuthProvider - FirebaseAuth - onAuthStateChanged - $e');
-    });*/
-
-
-
-
-
-
-
     userAuthSub = googleSignIn.onCurrentUserChanged
         .listen((GoogleSignInAccount? account) async {
-// #docregion CanAccessScopes
-      // In mobile, being authenticated means being authorized...
+      // Check Scopes: In mobile, being authenticated means being authorized...
       bool newIsAuthorized = account != null;
       // However, on web...
       if (kIsWeb && account != null) {
         print(' -- AuthProvider() - _googleSignIn.onCurrentUserChanged (web) -> account is null=${account == null}');
         newIsAuthorized = await googleSignIn.canAccessScopes(scopes);
       }
-// #enddocregion CanAccessScopes
 
       print(' -- AuthProvider() - _googleSignIn.onCurrentUserChanged (web) setState() - newIsAuthorized=${newIsAuthorized}');
       user = account;
-      notifyListeners();
-      //MyGlobals.user = _currentUser;
       isAuthorized = newIsAuthorized;
+      notifyListeners();
 
-      // Now that we know that the user can access the required scopes, the app
-      // can call the REST API.
+      // Now that we know that the user can access the required scopes, the app  can call the REST API.
       if (newIsAuthorized) {
-        //unawaited(_handleGetContact(account!)); //CONTACT DEMO
         await signInWithFirebase(account!); //from chatgpt
+
+        //Go to main page
+        CustomNavigationHelper.router.go(
+          CustomNavigationHelper.explorePath,
+        );
       } else {
         handleAuthorizeScopes();
       }
@@ -78,45 +64,25 @@ class AuthProvider with ChangeNotifier {
   void dispose() {
     if (userAuthSub != null) {
       userAuthSub.cancel();
-      //userAuthSub = null; //old code? seems no longer valid
     }
     super.dispose();
   }
 
-  /* //not needed? From Medium
-  bool get isAnonymous {
-    assert(user != null);
-    bool isAnonymousUser = true;
-    for (UserInfo info in user.providerData) {
-      if (info.providerId == "facebook.com" ||
-          info.providerId == "google.com" ||
-          info.providerId == "password") {
-        isAnonymousUser = false;
-        break;
-      }
-    }
-    return isAnonymousUser;
-  }*/
-
   bool get isAuthenticated {
     return user != null;
   }
+//#endregion
 
-  /* //not needed? From Medium
-  void signInAnonymously() {
-    FirebaseAuth.instance.signInAnonymously();
-  }*/
-
-  void signOut() {
-    //Go to login page
-    CustomNavigationHelper.router.go(
-      CustomNavigationHelper.signInPath,
-    );
-    FirebaseAuth.instance.signOut();
+//#region REGION: Sign in / out methods
+  void signInSilently() async {
+    print('AuthProvider.signInSilently()');
+    GoogleSignInAccount? silentlySignedInUser = await googleSignIn.signInSilently();
+    //if ()
   }
 
-  // Add the following function to sign in with Firebase
+  // Called when the current auth user changes (google sign in), so we automatically log into Firebase as well
   Future<void> signInWithFirebase(GoogleSignInAccount account) async {
+    print('AuthProvider.signInWithFirebase()');
     try {
       GoogleSignInAuthentication googleAuth = await account.authentication;
       AuthCredential credential = GoogleAuthProvider.credential(
@@ -134,45 +100,13 @@ class AuthProvider with ChangeNotifier {
   }
 
 
-
-
-  // Prompts the user to authorize `scopes`.
-  //
-  // This action is **required** in platforms that don't perform Authentication
-  // and Authorization at the same time (like the web).
-  //
-  // On the web, this must be called from an user interaction (button click).
-  // #docregion RequestScopes
-  Future<void> handleAuthorizeScopes() async {
-    final bool newIsAuthorized = await googleSignIn.requestScopes(scopes);
-    // #enddocregion RequestScopes
-    isAuthorized = newIsAuthorized;
-    notifyListeners();
-    // #docregion RequestScopes
-    if (newIsAuthorized) {
-      //unawaited(_handleGetContact(_currentUser!)); //CONTACT DEMO
-      //TODO: DO ON LOGIN
-    }
-
-    print('_handleAuthorizeScopes - newIsAuthorized=${newIsAuthorized}');
-    if (newIsAuthorized) {
-      //unawaited(_handleGetContact(account!)); //CONTACT DEMO
-      await signInWithFirebase(user!); //from chatgpt
-    }
-    // #enddocregion RequestScopes
-  }
-
-
-
-
-
   // This is the on-click handler for the Sign In button that is rendered by Flutter.
   //
   // On the web, the on-click handler of the Sign In button is owned by the JS
-  // SDK, so this method can be considered mobile only.
+  // SDK, so this method can be considered mobile only. - is this still true? not sure - > TODO: verify description
   // #docregion SignIn
   Future<void> handleSignIn() async {
-    print(' -- _handleSignIn()');
+    print('AuthProvider.handleSignIn()');
     try {
       GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
 
@@ -199,14 +133,50 @@ class AuthProvider with ChangeNotifier {
         isAuthorized = true;
         //Show success message
         SnackBarService.showSnackBar(content: 'You\'re signed in!');
-        //Go to main page
-        CustomNavigationHelper.router.go(
-          CustomNavigationHelper.explorePath,
-        );
         notifyListeners();
       }
     } catch (error) {
       print(error);
     }
   }
+
+
+  // Prompts the user to authorize `scopes`.
+  //
+  // This action is **required** in platforms that don't perform Authentication
+  // and Authorization at the same time (like the web).
+  //
+  // On the web, this must be called from an user interaction (button click).
+  Future<void> handleAuthorizeScopes() async {
+    print('AuthProvider.handleAuthorizeScopes()');
+    final bool newIsAuthorized = await googleSignIn.requestScopes(scopes);
+    isAuthorized = newIsAuthorized;
+    notifyListeners();
+
+
+    if (newIsAuthorized) {
+      await signInWithFirebase(user!); //from chatgpt
+    }
+  }
+
+  // Sign user out and go back to login page
+  void signOut() async {
+    try {
+      isLoggingOut = true;
+      print('AuthProvider.signOut()');
+      //Go to login page
+      CustomNavigationHelper.router.go(
+        CustomNavigationHelper.signInPath,
+      );
+      await FirebaseAuth.instance.signOut();
+      await googleSignIn.disconnect();
+      SnackBarService.showSnackBar(content: 'You\'re signed out!');
+      isLoggingOut = false;
+    } catch (e){
+      print(e);
+      isLoggingOut = false;
+    }
+
+  }
+//#endregion
 }
