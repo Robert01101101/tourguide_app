b09acdd676577122c3b709682c09d01ddf3441b8
Robert01101101 <hi@rmichels.com>
2024-07-30 13:53:01 -0700

    cleanup

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # tourguide_app
 
-Flutter app that helps guide users such as tourists around a city, with GPS and google maps driven info prompts.
-
-Links:
-
-- [GDoc with further links and notes](https://docs.google.com/document/d/163i37yum3VPPOEcq8DF8_kD8VAgpzDPC4ht41BfP4zg/edit?usp=sharing)
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
-
-## Notes
-
-- use `flutter run -d chrome --web-hostname localhost --web-port 60875 --web-browser-flag "--disable-web-security"` to run the app in debug mode, so that auth works (specific port required)
+A crossplatform Flutter app that helps users explore tours with navigation, information on places, and chat with an AI tourguide. A personal project currently in development.
--- lib/testing/debug_screen.dart
+++ lib/testing/debug_screen.dart
@@ -1,13 +1,11 @@
 import 'package:tourguide_app/testing/flutter_google_places_sample.dart';
 import 'package:tourguide_app/gemini_chat.dart';
 import 'package:tourguide_app/testing/second_screen_test.dart';
-import 'package:tourguide_app/testing/tour_creation_places_testing.dart';
 import 'package:tourguide_app/utilities/custom_import.dart';
 import 'package:tourguide_app/utilities/map_utils.dart';
 import 'package:flutter/material.dart';
 
 import 'google_maps_sample.dart';
-import 'google_maps_sample_draw_route.dart';
 import 'map.dart';
 
 //Debug Screen
@@ -60,12 +58,6 @@ class DebugScreenState extends State<DebugScreen> {
               ElevatedButton(onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const MapSampleDrawRoute()),
-                );
-              }, child: const Text("Map Routing")),
-              ElevatedButton(onPressed: () {
-                Navigator.push(
-                  context,
                   MaterialPageRoute(builder: (context) => const SecondRoute()),
                 );
               }, child: const Text("Route 2")),
@@ -75,12 +67,6 @@ class DebugScreenState extends State<DebugScreen> {
                   MaterialPageRoute(builder: (context) => const FlutterGooglePlacesSample()),
                 );
               }, child: const Text("Places")),
-              ElevatedButton(onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const CreateTourTesting()),
-                );
-              }, child: const Text("Tour creation places testing")),
               ElevatedButton(onPressed: () {
                 Navigator.push(
                   context,
